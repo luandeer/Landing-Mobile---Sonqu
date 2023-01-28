09fbe55268eb609d6fb83ac92660fdec4c6e41ae
alexander <alexanderperezreginaldo@gmail.com>
2023-01-27 20:38:46 -0500

    Subiendo el codigo fuente a github de la landing

---
+++ README.md
@@ -0,0 +1,1 @@
+# Landing-Mobile---Sonqu
---
+++ src/assets/js/api.js
@@ -0,0 +1,43 @@
+const API =
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCI_osfUfjsdyrq5Tp8lqhQA&part=snippet%2Cid&order=date&maxResults=4";
+const content = null || document.getElementById("content");
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    //7f18821b11mshfe3afcbbe81a8f1p1fd907jsnd6d05f2b9127
+    "X-RapidAPI-Key": "c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256",
+  },
+};
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items
+      .map(
+        (video) => `<a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank" class="link-video">
+      <div class="group">
+        <div>
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div>
+          <h3 class="text-descripcion">
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `
+      )
+      .slice(0, 4)
+      .join("")}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
